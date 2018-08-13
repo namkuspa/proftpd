2e952913232cdbd047189ff637a591acf058a96a
Jose Truyol <jose@namku.cl>
2018-08-12 21:23:24 -0300

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+echo "Configurando contenedor FTP"
+if ! [ `id -u $PROFTPD_USER 2>/dev/null || echo -1` -ge 0 ]; then 
+    echo "Creando usuario $PROFTPD_USER"
+    adduser -h /ftp -s /bin/false -H -u 82 -D -G $PROFTPD_USER $PROFTPD_USER
+fi
+echo "$PROFTPD_USER:$PROFTPD_PASSWORD" | chpasswd
+chown -R $PROFTPD_USER:$PROFTPD_USER /ftp
+/usr/local/sbin/proftpd -n -c /usr/local/etc/proftpd.conf
