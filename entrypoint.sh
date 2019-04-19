2966cf3242d08c8a554bd2382a054d0271a5cc42
jose-namku <40707142+jose-namku@users.noreply.github.com>
2019-04-19 14:26:26 -0400

    Acepta cualquier usuario

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 echo "Configurando contenedor FTP"
 if ! [ `id -u $PROFTPD_USER 2>/dev/null || echo -1` -ge 0 ]; then 
     echo "Creando usuario $PROFTPD_USER"
-    adduser -h /ftp -s /bin/false -H -u 82 -D -G $PROFTPD_USER $PROFTPD_USER
+    adduser -h /ftp -s /bin/false -H -u 82 -D -G $PROFTPD_USER ftp
 fi
 echo "$PROFTPD_USER:$PROFTPD_PASSWORD" | chpasswd
 chown -R $PROFTPD_USER:$PROFTPD_USER /ftp
